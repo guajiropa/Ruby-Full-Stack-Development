b7de4aabde4b28265bcff8031ae67c2c11017eb4
rjp-at-insight <robert.patterson@insight.com>
2020-09-23 23:28:04 -0400

    added procs.rb

---
+++ Intro/blocks.rb
@@ -0,0 +1,12 @@
+# block example with { }
+
+puts "\n\nusing { } curly-braces to define a code block:"
+
+3.times {|x| puts "x, the index val of the times method equals #{x}"}
+
+# using the do . . . end
+puts "\nusing do...end to define a code block:"
+
+3.times do |x|
+    puts "x, the index val of the times method equals #{x}"
+end
---
+++ Intro/procs.rb
@@ -0,0 +1,9 @@
+# using Proc.new
+
+my_proc = Proc.new { |x| puts "The argument value sent to proc is #{x}\." }
+puts "\nA proc object returns: '#{my_proc.class}' when class method is called."
+my_proc.call(100)
+
+# using the proc method
+my_proc = proc { |x| puts "The argument value sent to proc is #{x}\.\n\n" }
+my_proc.call(200)
